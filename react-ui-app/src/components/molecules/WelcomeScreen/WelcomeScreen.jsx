df92bf4e72e8d33b877903a749c057c4c01ab76c
anurag git <anurag@git>
2024-09-21 22:50:40 -0500

    initial commit

--- react-ui-app/src/components/molecules/WelcomeScreen/WelcomeScreen.jsx
+++ react-ui-app/src/components/molecules/WelcomeScreen/WelcomeScreen.jsx
@@ -1,72 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import './WelcomeScreen.scss'
-import { fetchEmployeeList, deleteEmployeeList } from '../../../utils/serviceUtils'
-import Button from '../../atoms/Button/Button'
+import EmployeeList from '../../atoms/EmployeeList/EmployeeList'
 
 const WelcomeScreen = () => {
-  const [empList, setEmpList] = useState([])
-
-  useEffect(() => {
-    fetchEmployeeList(setEmpList)
-  }, [])
-
-  const updateRecord = (id) => {
-    window.location.assign(`/add?id=${id}&action=update`)
-  }
-  const deleteRecord = (id) => {
-    deleteEmployeeList(id)
-    window.location.reload()
-  }
-  const addRecord = () => {
-    window.location.assign('/add')
-  }
-
-  const footerBtnArr = [
-    {
-      name: 'Update',
-      action: updateRecord,
-      type: 'update',
-    },
-    {
-      name: 'Delete',
-      action: deleteRecord,
-      type: 'delete',
-    },
-  ]
-  const btnArr = [
-    {
-      name: 'Add employee',
-      action: addRecord,
-      type: 'add',
-    },
-  ]
-
   return (
     <div className="welcomeScreen">
-      <div className="welcomeScreen__table">
-        <h1>Employee List:</h1>
-        <p>
-          <Button btnProp={btnArr} />
-        </p>
-        <table>
-          <tr>
-            <th>First name</th>
-            <th>Last name</th>
-            <th>Email</th>
-            <th>Actions</th>
-          </tr>
-          {empList.map((item, index) => (
-            <tr key={index}>
-              <td>{item.firstName}</td>
-              <td>{item.lastName}</td>
-              <td>{item.emailId}</td>
-              <td>
-                <Button btnProp={footerBtnArr} origin="actionBtn" ids={item.id} />
-              </td>
-            </tr>
-          ))}
-        </table>
-      </div>
+      <EmployeeList />
     </div>
   )
 }
